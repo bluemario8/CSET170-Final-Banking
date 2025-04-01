6a5710dd5ca58ea0f1a08bd377f7526a3341311d
bluemario8 <bluemario812@gmail.com>
2025-04-01 10:25:16 -0400

    Added database setup in .sql file and updated table translation some

---
+++ scripts/database_setup.sql
@@ -0,0 +1,47 @@
+CREATE DATABASE IF NOT EXISTS cset170final;
+USE cset170final;
+
+CREATE TABLE IF NOT EXISTS users (
+	acc_num INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL,
+    balance INT NOT NULL DEFAULT 0,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    ssn VARCHAR(9) NOT NULL,
+    phone_num INT,
+    CONSTRAINT chk_ssn_9_chars CHECK (LENGTH(ssn) = 9)
+);
+
+CREATE TABLE IF NOT EXISTS applications (
+    appli_num INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    ssn VARCHAR(9) NOT NULL,
+    phone_num int
+);
+
+CREATE TABLE IF NOT EXISTS addresses (
+    acc_num INT UNIQUE,
+    appli_num INT UNIQUE,
+    street_addr VARCHAR(100) NOT NULL,
+    city VARCHAR(100) NOT NULL,
+    state VARCHAR(100) NOT NULL,
+    zip_code INT NOT NULL,
+    country VARCHAR(2) NOT NULL,
+    FOREIGN KEY (acc_num) REFERENCES users(acc_num),
+    FOREIGN KEY (appli_num) REFERENCES applications(appli_num)
+);
+
+CREATE TABLE IF NOT EXISTS admin (
+	admin_id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(100) NOT NULL
+);
+
+INSERT INTO admin 
+	(username, password)
+VALUES
+	("admin", "password");
---
+++ scripts/nicholas_tests.sql
@@ -0,0 +1,26 @@
+USE cset170final;
+
+SELECT * FROM users;
+SELECT * FROM addresses;
+SELECT * FROM admin;
+
+INSERT INTO users 
+	(username, password, balance, first_name, last_name, ssn)
+VALUES 
+	-- ("bluemario8", "blue", 5, "nicholas", "N", "001352532"),
+	-- ("steve43", "stev", 0, "Steve", "Smith", "999999999"),
+    ("bill", "pass", 0, "Bill", "Stevens", "09");
+    
+INSERT INTO applications
+	(username, password, first_name, last_name, ssn)
+VALUES
+	("Jim", "power", "Jim", "Browning", "91043251");
+    
+INSERT INTO addresses
+	(acc_num, appli_num, street_addr, city, state, zip_code, country)
+VALUES
+	(1, NULL, "123 Street St.", "Street City", "New York", 12345, "US");
+    
+    
+    
+    
