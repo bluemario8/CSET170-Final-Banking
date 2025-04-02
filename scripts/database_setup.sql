3344e288109ff585dc68e4202d1f11d9c5164fd4
EllieMott <elizabethmott1326@gmail.com>
2025-04-02 10:36:48 -0400

    +loggedin check in sql file

--- scripts/database_setup.sql
+++ scripts/database_setup.sql
@@ -45,7 +45,8 @@ CREATE TABLE IF NOT EXISTS loggedin (
     acc_num INT,
     admin_id INT,
     FOREIGN KEY (acc_num) REFERENCES users(acc_num),
-    FOREIGN KEY (admin_id) REFERENCES admin(admin_id)
+    FOREIGN KEY (admin_id) REFERENCES admin(admin_id),
+    CHECK (acc_num IS NULL OR admin_id IS NULL)
 );
 
 INSERT INTO admin 
@@ -55,6 +56,7 @@ VALUES
     
 INSERT INTO users (acc_num, username, first_name, last_name, phone_num, password, ssn) VALUES(001, 'ddzidzic', 'Daedalus', 'Dzidzic', '1234567890', 'password', '333222444');
 INSERT INTO loggedin (acc_num) VALUES(001);
-INSERT INTO addresses (acc_num, street_addr, city, state, zip_code, country) VALUES (1, '123 Example St.', 'City', 'PA', 'USA');
+INSERT INTO addresses (acc_num, street_addr, city, state, zip_code, country) VALUES (1, '123 Example St.', 'City', 'PA', '12345', 'US');
 select * from users;
 select * from loggedin;
+select * from addresses;
