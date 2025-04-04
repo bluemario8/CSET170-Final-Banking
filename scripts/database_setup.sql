960b3a662f5b480e244cca3e1026afb1ae3bca24
EllieMott <elizabethmott1326@gmail.com>
2025-04-04 15:31:58 -0400

    changed sql file so username in users is not unique, allowing multiple accounts per user

--- scripts/database_setup.sql
+++ scripts/database_setup.sql
@@ -3,7 +3,7 @@ USE cset170final;
 
 CREATE TABLE IF NOT EXISTS users (
 	acc_num INT PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(50) NOT NULL UNIQUE,
+    username VARCHAR(50) NOT NULL,
     password VARCHAR(300) NOT NULL,
     balance INT(255) NOT NULL DEFAULT 0,
     first_name VARCHAR(50) NOT NULL,
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS users (
     phone_num VARCHAR(15),
     CONSTRAINT chk_ssn_9_chars CHECK (LENGTH(ssn) = 9)
 );
-ALTER TABLE users AUTO_INCREMENT=550850;
+ALTER TABLE users AUTO_INCREMENT=578060;
 
 CREATE TABLE IF NOT EXISTS applications (
     appli_num INT PRIMARY KEY AUTO_INCREMENT,
@@ -43,18 +43,17 @@ CREATE TABLE IF NOT EXISTS admin (
 -- If you ran the old admin table create and don't want to delete loggedin
 -- ALTER TABLE admin ADD CONSTRAINT unique_username UNIQUE (username);
 
-CREATE TABLE IF NOT EXISTS loggedin (
-    acc_num INT,
-    admin_id INT,
-    FOREIGN KEY (acc_num) REFERENCES users(acc_num), 
-    FOREIGN KEY (admin_id) REFERENCES admin(admin_id),
-    CHECK (acc_num IS NULL OR admin_id IS NULL)
-);
+-- CREATE TABLE IF NOT EXISTS loggedin (
+--     acc_num INT,
+--     admin_id INT,
+--     FOREIGN KEY (acc_num) REFERENCES users(acc_num), 
+--     FOREIGN KEY (admin_id) REFERENCES admin(admin_id),
+--     CHECK (acc_num IS NULL OR admin_id IS NULL)
+-- );
 
 CREATE TABLE IF NOT EXISTS loggedin (
     username VARCHAR(255),
     admin_id INT,
-    FOREIGN KEY (username) REFERENCES users(username),
     FOREIGN KEY (admin_id) REFERENCES admin(admin_id),
     CHECK (username IS NULL OR admin_id IS NULL)
 );
@@ -66,6 +65,7 @@ VALUES
 	("admin", "password");
     
 INSERT INTO users (username, balance, first_name, last_name, phone_num, password, ssn) VALUES('ddzidzic', '95000', 'Daedalus', 'Dzidzic', '1234567890', 'password', '333222444');
+INSERT INTO users (username, balance, first_name, last_name, phone_num, password, ssn) VALUES('ddzidzic', '12467', 'Daedalus', 'Dzidzic', '1234567890', 'password', '333222444');
 INSERT INTO users (username, balance, first_name, last_name, phone_num, password, ssn) VALUES ('mmalova', '780000', 'Maya', 'Malova', '0987654321', 'password', '333222333');
 
 INSERT INTO loggedin (username) VALUES('ddzidzic');
@@ -74,7 +74,7 @@ select * from users;
 select * from loggedin;
 select * from addresses;
 
-INSERT INTO loggedin 
-VALUES (NULL, NULL);
+-- INSERT INTO loggedin 
+-- VALUES (NULL, NULL);
 
 -- drop database cset170final;
