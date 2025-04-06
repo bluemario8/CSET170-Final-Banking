fd07717298bf0d6f85d44a2a82eae6f84ca25c64
EllieMott <elizabethmott1326@gmail.com>
2025-04-06 16:05:20 -0400

    changes to sql file for user / address values

--- scripts/database_setup.sql
+++ scripts/database_setup.sql
@@ -4,7 +4,7 @@ USE cset170final;
 CREATE TABLE IF NOT EXISTS users (
 	acc_num INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(50) NOT NULL,
-    password VARCHAR(255) NOT NULL,
+    password VARCHAR(300) NOT NULL,
     balance INT(255) NOT NULL DEFAULT 0,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
@@ -17,7 +17,7 @@ ALTER TABLE users AUTO_INCREMENT=578060;
 CREATE TABLE IF NOT EXISTS applications (
     appli_num INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
+    password VARCHAR(300) NOT NULL,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     ssn VARCHAR(9) NOT NULL,
@@ -48,25 +48,60 @@ CREATE TABLE IF NOT EXISTS loggedin (
 );
 -- drop table loggedin;
 
+CREATE TABLE IF NOT EXISTS transactions (
+	transaction_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    acc_num INT NOT NULL,
+    type VARCHAR(8),
+    related_acc INT NOT NULL,
+    amount INTEGER NOT NULL,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+    description VARCHAR(100),
+    FOREIGN KEY (acc_num) REFERENCES users(acc_num),
+    FOREIGN KEY (related_acc) REFERENCES users(acc_num)
+);
+
 INSERT INTO admin 
 	(username, password)
 VALUES
 	("admin", "$+&091dmk_qdRDR@$+50_;oDiieR`~8D2q//KP=RR=88_=*G5KREqdko5Jk$;k9+533K;2`Eo;J/G`@_d35G&kqqdE@=P1`+PDeP0+=?LP;k*2$m2DK23E?_L+RR/E&`GJq13q=k+oid1o59iL0!8/5D0EKLo/*5$=R@;32_?2?dP`=Rk_iD`/J+Pq~8~oi2R8dG9K;i?P9?=#~L?k0*@i=+`159mG`@~&_m9i8$@k*9!mi?o3");
 -- admin password is 'password'
-    
-INSERT INTO users (acc_num, username, first_name, last_name, phone_num, password, ssn) VALUES(001, 'ddzidzic', 'Daedalus', 'Dzidzic', '1234567890', '$+&091dmk_qdRDR@$+50_;oDiieR`~8D2q//KP=RR=88_=*G5KREqdko5Jk$;k9+533K;2`Eo;J/G`@_d35G&kqqdE@=P1`+PDeP0+=?LP;k*2$m2DK23E?_L+RR/E&`GJq13q=k+oid1o59iL0!8/5D0EKLo/*5$=R@;32_?2?dP`=Rk_iD`/J+Pq~8~oi2R8dG9K;i?P9?=#~L?k0*@i=+`159mG`@~&_m9i8$@k*9!mi?o3', '333222444');
--- this user password is 'password'
-INSERT INTO users (username, balance, first_name, last_name, phone_num, password, ssn) VALUES('ddzidzic', '95000', 'Daedalus', 'Dzidzic', '1234567890', 'password', '333222444');
-INSERT INTO users (username, balance, first_name, last_name, phone_num, password, ssn) VALUES('ddzidzic', '12467', 'Daedalus', 'Dzidzic', '1234567890', 'password', '333222444');
-INSERT INTO users (username, balance, first_name, last_name, phone_num, password, ssn) VALUES ('mmalova', '780000', 'Maya', 'Malova', '0987654321', 'password', '333222333');
 
-INSERT INTO loggedin (username) VALUES('ddzidzic');
-INSERT INTO addresses (username, street_addr, city, state, zip_code) VALUES ('ddzidzic', '123 Example St.', 'City', 'PA', '12345');
+INSERT INTO users (username, password, balance, first_name, last_name, ssn, phone_num)
+	VALUES ('ddzidzic', '$+&2q9e~*$1+JR=G_#K$8`!_/k~9?3#oEJ/`dLe*D$5?_GR#kPEk2JK2;kdE8#$2mmd/=G5#EK0dR=$3RG18L20J0~q;Q`#2`0~=e@Gq_`2@+JRDQ5i/3~*;L`95&@mq/D=1`ei&D*~kQKdKR1d+$k2R!5m2_RQo_L=KQ5@J$=@93R~2i`;#J`1J8Km`#*`D@11qq_o/&Q`+e`&3?`EDio9?*K55iL82Pm`;&o1/GJi@_mo/DQ',
+			0, 'Daedalus', 'Dzidzic', '000000000', 1112223333),
+            ('ddzidzic', '$+&2q9e~*$1+JR=G_#K$8`!_/k~9?3#oEJ/`dLe*D$5?_GR#kPEk2JK2;kdE8#$2mmd/=G5#EK0dR=$3RG18L20J0~q;Q`#2`0~=e@Gq_`2@+JRDQ5i/3~*;L`95&@mq/D=1`ei&D*~kQKdKR1d+$k2R!5m2_RQo_L=KQ5@J$=@93R~2i`;#J`1J8Km`#*`D@11qq_o/&Q`+e`&3?`EDio9?*K55iL82Pm`;&o1/GJi@_mo/DQ',
+			0, 'Daedalus', 'Dzidzic', '000000000', 1112223333),
+            ('spetocs', '$+&2q9e~*$1+JR=G_#K$8`!_/k~9?3#oEJ/`dLe*D$5?_GR#kPEk2JK2;kdE8#$2mmd/=G5#EK0dR=$3RG18L20J0~q;Q`#2`0~=e@Gq_`2@+JRDQ5i/3~*;L`95&@mq/D=1`ei&D*~kQKdKR1d+$k2R!5m2_RQo_L=KQ5@J$=@93R~2i`;#J`1J8Km`#*`D@11qq_o/&Q`+e`&3?`EDio9?*K55iL82Pm`;&o1/GJi@_mo/DQ',
+            0, 'Sujay', 'Petocs', '333333333', 4445556666),
+            ('mmalova', '$+&2q9e~*$1+JR=G_#K$8`!_/k~9?3#oEJ/`dLe*D$5?_GR#kPEk2JK2;kdE8#$2mmd/=G5#EK0dR=$3RG18L20J0~q;Q`#2`0~=e@Gq_`2@+JRDQ5i/3~*;L`95&@mq/D=1`ei&D*~kQKdKR1d+$k2R!5m2_RQo_L=KQ5@J$=@93R~2i`;#J`1J8Km`#*`D@11qq_o/&Q`+e`&3?`EDio9?*K55iL82Pm`;&o1/GJi@_mo/DQ',
+            0, 'Maya', 'Malova', '111111111', 2223334444),
+            ('mmalova', '$+&2q9e~*$1+JR=G_#K$8`!_/k~9?3#oEJ/`dLe*D$5?_GR#kPEk2JK2;kdE8#$2mmd/=G5#EK0dR=$3RG18L20J0~q;Q`#2`0~=e@Gq_`2@+JRDQ5i/3~*;L`95&@mq/D=1`ei&D*~kQKdKR1d+$k2R!5m2_RQo_L=KQ5@J$=@93R~2i`;#J`1J8Km`#*`D@11qq_o/&Q`+e`&3?`EDio9?*K55iL82Pm`;&o1/GJi@_mo/DQ',
+            0, 'Maya', 'Malova', '111111111', 2223334444),
+            ('dpolakova', '$+&2q9e~*$1+JR=G_#K$8`!_/k~9?3#oEJ/`dLe*D$5?_GR#kPEk2JK2;kdE8#$2mmd/=G5#EK0dR=$3RG18L20J0~q;Q`#2`0~=e@Gq_`2@+JRDQ5i/3~*;L`95&@mq/D=1`ei&D*~kQKdKR1d+$k2R!5m2_RQo_L=KQ5@J$=@93R~2i`;#J`1J8Km`#*`D@11qq_o/&Q`+e`&3?`EDio9?*K55iL82Pm`;&o1/GJi@_mo/DQ',
+            0, 'Darina', 'Polakova', '222222222', 3334445555),
+            ('spetocs', '$+&2q9e~*$1+JR=G_#K$8`!_/k~9?3#oEJ/`dLe*D$5?_GR#kPEk2JK2;kdE8#$2mmd/=G5#EK0dR=$3RG18L20J0~q;Q`#2`0~=e@Gq_`2@+JRDQ5i/3~*;L`95&@mq/D=1`ei&D*~kQKdKR1d+$k2R!5m2_RQo_L=KQ5@J$=@93R~2i`;#J`1J8Km`#*`D@11qq_o/&Q`+e`&3?`EDio9?*K55iL82Pm`;&o1/GJi@_mo/DQ',
+            0, 'Sujay', 'Petocs', '333333333', 4445556666),
+            ('spetocs', '$+&2q9e~*$1+JR=G_#K$8`!_/k~9?3#oEJ/`dLe*D$5?_GR#kPEk2JK2;kdE8#$2mmd/=G5#EK0dR=$3RG18L20J0~q;Q`#2`0~=e@Gq_`2@+JRDQ5i/3~*;L`95&@mq/D=1`ei&D*~kQKdKR1d+$k2R!5m2_RQo_L=KQ5@J$=@93R~2i`;#J`1J8Km`#*`D@11qq_o/&Q`+e`&3?`EDio9?*K55iL82Pm`;&o1/GJi@_mo/DQ',
+            0, 'Sujay', 'Petocs', '333333333', 4445556666),
+            ('ddzidzic', '$+&2q9e~*$1+JR=G_#K$8`!_/k~9?3#oEJ/`dLe*D$5?_GR#kPEk2JK2;kdE8#$2mmd/=G5#EK0dR=$3RG18L20J0~q;Q`#2`0~=e@Gq_`2@+JRDQ5i/3~*;L`95&@mq/D=1`ei&D*~kQKdKR1d+$k2R!5m2_RQo_L=KQ5@J$=@93R~2i`;#J`1J8Km`#*`D@11qq_o/&Q`+e`&3?`EDio9?*K55iL82Pm`;&o1/GJi@_mo/DQ',
+			0, 'Daedalus', 'Dzidzic', '000000000', 1112223333),
+            ('dpolakova', '$+&2q9e~*$1+JR=G_#K$8`!_/k~9?3#oEJ/`dLe*D$5?_GR#kPEk2JK2;kdE8#$2mmd/=G5#EK0dR=$3RG18L20J0~q;Q`#2`0~=e@Gq_`2@+JRDQ5i/3~*;L`95&@mq/D=1`ei&D*~kQKdKR1d+$k2R!5m2_RQo_L=KQ5@J$=@93R~2i`;#J`1J8Km`#*`D@11qq_o/&Q`+e`&3?`EDio9?*K55iL82Pm`;&o1/GJi@_mo/DQ',
+            0, 'Darina', 'Polakova', '222222222', 3334445555);
+-- all user passwords are TestPass01!
+
+INSERT INTO addresses (username, street_addr, city, state, zip_code)
+VALUES ('ddzidzic', '54 Sunshine Avenue', 'Belltown', 'AZ', '86118'),
+		('mmalova', '162 Valley Street', 'Gap', 'PA', '17535'),
+        ('dpolakova', '1955 Jana Drive', 'Clarksville', 'TN', '37042'),
+        ('spetocs', '270 North Whealkate Drive', 'South Range', 'MI', '49963');
+
+INSERT INTO loggedin (username, admin_id) VALUES (NULL, NULL);
+
 select * from users;
 select * from loggedin;
 select * from addresses;
+select * from transactions;
 
--- INSERT INTO loggedin 
--- VALUES (NULL, NULL);
+-- UPDATE loggedin SET username = NULL, admin_id = NULL;
 
 -- drop database cset170final;
